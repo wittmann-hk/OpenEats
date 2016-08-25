273205cd2c2047da67fa966489fbc5fa40e0a62b
RyanNoelk <ryannoelk@gmail.com>
2016-08-25 09:02:21 +0200

    new wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-"""
-WSGI config for base project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+root = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, root)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'nameofyourproject.settings'
 
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
