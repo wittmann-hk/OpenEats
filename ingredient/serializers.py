003775f9fc9b21f4e4620db5d43e4f555ec96d69
ryan.noelk <ryannoelk@gmail.com>
2016-09-19 13:44:08 -0400

    cleaning up the ingredient api

--- ingredient/serializers.py
+++ ingredient/serializers.py
@@ -5,22 +5,21 @@ from models import Ingredient
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
-        exclude = ('slug', 'pub_date',)
+        fields = '__all__'
 
     def create(self, validated_data):
         """
         Create and return a new `Snippet` instance, given the validated data.
         """
-        return Snippet.objects.create(**validated_data)
+        return Ingredient.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         """
         Update and return an existing `Snippet` instance, given the validated data.
         """
         instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
+        instance.quantity = validated_data.get('quantity', instance.quantity)
+        instance.measurement = validated_data.get('measurement', instance.measurement)
+        instance.preparation = validated_data.get('preparation', instance.preparation)
         instance.save()
         return instance
--- ingredient/views.py
+++ ingredient/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from models import Ingredient
 import json
 from serializers import IngredientSerializer
-from rest_framework import permissions
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
+from permissions import IsOwnerOrReadOnly
 
 
 class IngredientViewSet(viewsets.ModelViewSet):
@@ -13,17 +13,5 @@ class IngredientViewSet(viewsets.ModelViewSet):
     """
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def autocomplete_ing(request):
-        """Used to auto complete ingredient names on the recipe form. This view is called by a jquery script
-           returns json list of ten ingredients found
-        """
-
-        q = request.GET.get('term', '')
-        ing_list = Ingredient.objects.filter(title__istartswith=q).values_list('title').distinct().order_by('title')[:10]
-        results = []
-        for ing_item in ing_list:
-            results.append(" ".join(ing_item))
-        json_obj = json.dumps(results)
-        return HttpResponse(json_obj, content_type="application/json")
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
