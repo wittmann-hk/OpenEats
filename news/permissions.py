3f91fd1df23f9cf7f01b8754a6024c2bc1257c28
ryan.noelk <ryannoelk@gmail.com>
2016-09-19 13:57:44 -0400

    fixing news permissions

--- news/permissions.py
+++ news/permissions.py
@@ -9,7 +9,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.IsAdminUser:
+        if request.method in permissions.SAFE_METHODS:
             return True
 
         # Write permissions are only allowed to owners.
