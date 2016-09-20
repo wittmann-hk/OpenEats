201342431eba5afbaba0b068594607797b5b0466
ryan.noelk <ryannoelk@gmail.com>
2016-09-19 20:28:43 -0400

    removing feeds

--- feed/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from views import RecentRecipesFeed,TopRecipesFeed
-
-urlpatterns = (
-    url(r'^recent/$', RecentRecipesFeed()),
-    url(r'^top/$', TopRecipesFeed()),
-)
