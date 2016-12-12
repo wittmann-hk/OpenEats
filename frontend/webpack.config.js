042a79091a94931cc385b9fbea3acf51900f7c1a
Ryan Noelk <ryannoelk@gmail.com>
2016-12-12 14:30:35 -0500

    using an env var to load the API url (#42)

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,7 +9,8 @@ function getPlugins() {
 
   plugins.push(new webpack.DefinePlugin({
     'process.env': {
-      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'API_URL': JSON.stringify(process.env.API_URL)
     }
   }));
 
