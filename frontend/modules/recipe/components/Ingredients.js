665d1c4ad11916a56f7d30e1e3bf925eedfa9f0d
ryan.noelk <ryannoelk@gmail.com>
2017-01-26 22:14:51 -0500

    factions

--- frontend/modules/recipe/components/Ingredients.js
+++ frontend/modules/recipe/components/Ingredients.js
@@ -3,6 +3,12 @@ import request from 'superagent';
 
 import { serverURLs } from '../../common/config'
 
+// configure the default type of numbers as Fractions
+var math = require('mathjs');
+math.config({
+  number: 'Fraction'   // Default type of number:
+});
+
 export default React.createClass({
   loadRecipeFromServer: function() {
     var url = serverURLs.ingredient + "&recipe="+ this.props.recipe_id;
@@ -23,11 +29,24 @@ export default React.createClass({
   componentDidMount: function() {
     this.loadRecipeFromServer();
   },
+  printRatio: function(value) {
+    // Format the value then print it as a fraction.
+    value = math.fraction(value);
+    console.log(value);
+    if (value.d == 1) {
+      return value.n;
+    } else if (value.n > value.d) {
+      return math.floor(value.n/value.d).toString() +
+        " " + (value.n%value.d).toString() + "/" + value.d
+    }
+    return math.format(value, {fraction: 'ratio'});
+  },
   render: function() {
+    var printRatio = this.printRatio;
     var ingredients = this.state.data.map(function(ingredient) {
       return (
         <li className="ingredient" key={ ingredient.id }>
-          { ingredient.quantity }&nbsp;
+          { printRatio(ingredient.quantity) }&nbsp;
           { ingredient.measurement }&nbsp;
           { ingredient.title }
         </li>
