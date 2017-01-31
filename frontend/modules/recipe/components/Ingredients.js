f682f0351b635c007c22cbc90293ea32cb3aa9ac
Hermann Käser <hermann.kaser@gmail.com>
2017-01-31 17:47:09 +0000

    Fixes loading of recipe content after clicking sidebar link

--- frontend/modules/recipe/components/Ingredients.js
+++ frontend/modules/recipe/components/Ingredients.js
@@ -4,25 +4,16 @@ import request from 'superagent';
 import { serverURLs } from '../../common/config'
 
 export default React.createClass({
-  loadRecipeFromServer: function() {
-    var url = serverURLs.ingredient + "&recipe="+ this.props.recipe_id;
-    request
-      .get(url)
-      .type('json')
-      .end((err, res) => {
-        if (!err && res) {
-          this.setState({ data: res.body.results });
-        } else {
-          console.error(url, err.toString());
-        }
-      })
-  },
   getInitialState: function() {
-    return { data: [] };
+    return {
+      data: []
+    };
   },
-  componentDidMount: function() {
-    this.loadRecipeFromServer();
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({data: nextProps.data});
   },
+
   render: function() {
     var ingredients = this.state.data.map(function(ingredient) {
       return (
