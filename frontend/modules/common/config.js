31e18bb0bc75ae4d31b01ef6aa50186acd2dcfbb
ryan.noelk <ryannoelk@gmail.com>
2017-02-22 22:10:52 -0500

    adding back json request in the api calls

--- frontend/modules/common/config.js
+++ frontend/modules/common/config.js
@@ -7,16 +7,15 @@ var apiUrl = apiHost + '/api/v1';
 
 export var serverURLs = {
   auth_token: apiUrl + '/accounts/obtain-auth-token/',
-  browse: apiUrl + '/recipe/recipes/?fields=id,title,pub_date,rating,photo_thumbnail,info',
+  browse: apiUrl + '/recipe/recipes/?fields=id,title,pub_date,rating,photo_thumbnail,info&format=json',
   mini_browse: apiUrl + '/recipe/mini-browse/?format=json',
-  //create: apiUrl + '/recipe/recipes/',
-  cuisine: apiUrl + '/recipe_groups/cuisine/',
-  course: apiUrl + '/recipe_groups/course/',
+  cuisine: apiUrl + '/recipe_groups/cuisine/?format=json',
+  course: apiUrl + '/recipe_groups/course/?format=json',
   tag: apiUrl + '/recipe_groups/tag/?format=json',
   ingredient: apiUrl + '/ingredient/ingredient/?format=json',
   direction: apiUrl + '/recipe/direction/?format=json',
   news: apiUrl + '/news/entry/?format=json',
-  recipe: apiUrl + '/recipe/recipes/',
+  recipe: apiUrl + '/recipe/recipes/?format=json',
 };
 
 export var measurements = [
